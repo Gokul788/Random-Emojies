13bb1dbcbeeeb997bca089f35593e3534d9be62b
gokul788 <gokul774777@gmail.com>
2024-12-26 12:36:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const emojiArray = [
+    "😆",
+  "😅",
+  "🤣",
+  "😂",
+  "😀",
+  "🤑",
+  "🤨",
+  "🙂",
+  "😊",
+  "😗",
+  "😛",
+  "😏",
+  "🤥",
+  "😴",
+  "🥺",
+  "😧",
+  "😇",
+  "😳",
+  "🙃",
+  "🥴",
+  "🧐",
+  "🤨",
+  "😒",
+  "🤔",
+  "🤭",
+  "🥰",
+  "🤐",
+  "😄",
+  "🤔",
+  "🤪",
+  "🥲",
+  "😃",
+  "😁",
+  "😬",
+]
+
+let container = document.querySelector('.emoji_container');
+let button = document.querySelector('button');
+
+const generateEmoji = ()=>{
+
+    const randomNumber = Math.floor(Math.random() * emojiArray.length);
+    container.textContent = emojiArray[randomNumber]
+      
+}
+button.addEventListener('click',generateEmoji);
